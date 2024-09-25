3c42d6873326fa1bf1c25d472f3ce15913e1bfe2
MukeshAbhi <abhimukesh284@gmail.com>
2024-09-25 11:33:35 +0530

    mongo need to be added

---
+++ week 4/backend/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const { createTodo,updateTodo } = require('./types');
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/todo',(req,res)=>{
+    const createLoad  = req.body;
+    const parsedLoad = createTodo.safeParse(createLoad);
+    if(!parsedLoad.success){
+        res.status(411).json({
+            msg: "You sent the wrong inputs"
+        })
+    }
+})
+
+app.get('/todos',(req,res)=> {
+
+})
+
+app.put('/completed',(req,res)=>{
+    const id = req.body;
+    const parsedId = updateTodo.safeParse(id);
+    if(!parsedId.success){
+        res.status(411).json({
+            msg: "You sent the wrong inputs"
+        })
+    }
+})
